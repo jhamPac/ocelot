2542913908c37ef6943c518fad472f8461bc15dd
jhamPac <cloud@kevia.me>
2021-09-14 20:04:37 -0700

    feat: implement cli for the server

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,38 @@
 module Main where
 
-import           Data.Semigroup ((<>))
+import           Control.Monad       (join)
+import           Data.Semigroup      ((<>))
 import           Lib
+import           Options.Applicative (CommandFields, Mod, Parser, ParserInfo,
+                                      argument, command, execParser, idm, info,
+                                      str, subparser)
+
+type Host = String
+type Port = Integer
+
+data Command = Serve Host Port
+
+newtype Options = Options {cmd :: Command}
+
+server :: IO ()
+server = putStrLn "Ocelot...Server"
+
+opts :: Parser (IO ())
+opts = subparser commands
+    where
+        serverAction :: Parser (IO ())
+        serverAction = pure server
+
+        serverCmd :: ParserInfo (IO ())
+        serverCmd = info serverAction idm
+
+        commands :: Mod CommandFields (IO ())
+        commands = command "server" serverCmd
+
+
 
 main :: IO ()
-main = someFunc
+main = join $ execParser parser
+    where
+        parser :: ParserInfo (IO ())
+        parser = info opts idm
