e6a21c06728c4d909960ef5dfb6c635434de8819
jhamPac <cloud@kevia.me>
2021-09-15 18:41:51 -0700

    feat: model config types

---
+++ src/Config.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Config where
+
+import           Data.Functor.Identity (Identity (Identity))
+
+type Host = String
+type Port = Integer
+
+type CompleteAppConfig = AppConfig Identity
+type PartialAppConfig = AppConfig Maybe
+
+data AppConfig f = AppConfig {acHost :: f Host, acPort :: f Port}
+
+class HasDefaultValue a where
+    defaultValue :: a
+
+defaultHost :: Host
+defaultHost = "localhost"
+
+defaultPort :: Port
+defaultPort = 5000
+
+instance HasDefaultValue (AppConfig Identity) where
+    defaultValue = AppConfig (Identity defaultHost) (Identity defaultPort)
+
+instance HasDefaultValue (AppConfig Maybe) where
+    defaultValue = AppConfig (Just defaultHost) (Just defaultPort)
+
+
+
