0138aa6ae4168094bd2b8daf61d6cb41b179c4d4
jhamPac <cloud@kevia.me>
2021-09-16 21:25:44 -0700

    chore: add dependencies for system file paths

--- src/Config.hs
+++ src/Config.hs
@@ -1,19 +1,49 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 
 module Config where
 
-import           Data.Aeson            (eitherDecode, withObject, (.:))
+import           Data.Aeson            (FromJSON (parseJSON), eitherDecode,
+                                        toJSON)
 import           Data.Bifunctor        (first)
 import           Data.ByteString.Lazy  as DBL
 import           Data.Functor.Identity (Identity (Identity))
+import           GHC.Generics
+import           System.FilePath       as FP
+import           Text.Parsec.Error     (ParseError)
 
 type Host = String
 type Port = Integer
 
-type CompleteAppConfig = AppConfig Identity
-type PartialAppConfig = AppConfig Maybe
+data AppConfig f = AppConfig
+    { acHost :: f Host
+    , acPort :: f Port
+    } deriving (Generic)
 
-data AppConfig f = AppConfig {acHost :: f Host, acPort :: f Port}
+type CompleteAppConfig = AppConfig Identity
+deriving instance Generic CompleteAppConfig
+deriving instance Eq CompleteAppConfig
+deriving instance Show CompleteAppConfig
+deriving instance FromJSON CompleteAppConfig
+
+type PartialAppConfig = AppConfig Maybe
+deriving instance Generic PartialAppConfig
+deriving instance Eq PartialAppConfig
+deriving instance Show PartialAppConfig
+deriving instance FromJSON PartialAppConfig
+
+data ConfigurationError = ConfigParseError String
+                        | TOMLParseError ParseError
+                        | InvalidConfigError String
+                        | InvalidPath FP.FilePath String
+                            deriving (Eq)
+
+class (FromJSON cfg) => FromJSONFile cfg where
+    fromJSONFile :: FilePath -> IO (Either ConfigurationError cfg)
 
 class HasDefaultValue a where
     defaultValue :: a
@@ -29,6 +59,14 @@ instance HasDefaultValue (AppConfig Identity) where
 
 instance HasDefaultValue (AppConfig Maybe) where
     defaultValue = AppConfig (Just defaultHost) (Just defaultPort)
+
+instance FromJSON PartialAppConfig where
+    parseJSON = withObject "cfg" parseObj
+        where
+            parseObj :: Object -> Parser PartialAppConfig
+            parseObj obj = obj .: "host"
+                            >>= \host -> obj .: "port"
+                            >>= \port -> pure $ AppConfig {host=host, port=port}
 
 
 
